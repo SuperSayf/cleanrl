1630110292ef04887b144886c6aacc80d7bc56a2
Sayfullah <sayfullah.jumoorty@live.com>
2024-05-19 14:07:59 +0200

    exp

--- frame_skip_exp/run.sh
+++ frame_skip_exp/run.sh
@@ -14,9 +14,9 @@ job_name_2=Atari_2
 job_name_3=Atari_3
 job_name_4=Atari_4
 
-total_timestepstotal_timesteps=10000000
+total_timesteps=10000000
 
-bash slurm.sh $frame_skip_exp_0 $job_name_0 $total_timesteps
+sbatch slurm.batch $frame_skip_exp_0 $job_name_0 $total_timesteps
 # sbatch slurm.batch $frame_skip_exp_1 $job_name_1 $total_timesteps
 # sbatch slurm.batch $frame_skip_exp_2 $job_name_2 $total_timesteps
 # sbatch slurm.batch $frame_skip_exp_3 $job_name_3 $total_timesteps
