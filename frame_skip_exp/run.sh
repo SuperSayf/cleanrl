4b5afaa1b6cbd672dd376052f9bd5b2e3b8f9f9a
Sayfullah <sayfullah.jumoorty@live.com>
2024-06-08 14:47:00 +0200

    website

--- frame_skip_exp/run.sh
+++ frame_skip_exp/run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# This script is used to submit the batch files to the cluster
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <frame_skip> <total_timesteps>"
+    exit 1
+fi
 
+# Set the variables from the command line arguments
+FRAME_SKIP=$1
+TOTAL_TIMESTEPS=$2
+
+# Export the variables to make them available to the batch script
+export FRAME_SKIP
+export TOTAL_TIMESTEPS
+
+# Submit the batch file
 sbatch job1.batch
