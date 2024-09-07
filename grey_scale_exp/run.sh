a34659faaef4c97a1b7645ed3c3b723b726d6cd8
sayf-wits <sayfullah.jumoorty@live.com>
2024-09-07 18:35:33 +0200

    paths

--- grey_scale_exp/run.sh
+++ grey_scale_exp/run.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home-mscluster/sjumoorty2/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh"
+# Check if the first conda path exists
+if [ -d "/home-mscluster/sjumoorty2/anaconda3" ]; then
+    # Setup for the first conda installation
+    __conda_setup="$('/home-mscluster/sjumoorty2/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
     else
-        export PATH="/home-mscluster/sjumoorty2/anaconda3/bin:$PATH"
+        if [ -f "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home-mscluster/sjumoorty2/anaconda3/bin:$PATH"
+        fi
     fi
+    unset __conda_setup
+# If the first path doesn't exist, check the second conda path
+elif [ -d "/home-mscluster/mdawood/miniconda3" ]; then
+    # Setup for the second conda installation
+    __conda_setup="$('/home-mscluster/mdawood/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/home-mscluster/mdawood/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "/home-mscluster/mdawood/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home-mscluster/mdawood/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+else
+    echo "Error: Neither conda installation found. Please check your conda paths."
+    exit 1
 fi
-unset __conda_setup
-# <<< conda initialize <<<
 
 cd ~/cleanrl/grey_scale_exp
 
