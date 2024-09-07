18cf4e3a08de92362337c726d4331cde59cb04a3
sayf-wits <sayfullah.jumoorty@live.com>
2024-09-07 18:27:59 +0200

    conda again

--- combined_exp/run.sh
+++ combined_exp/run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-# Try the original conda setup
-__conda_setup="$('/home-mscluster/sjumoorty2/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh"
+# Check if the first conda path exists
+if [ -d "/home-mscluster/sjumoorty2/anaconda3" ]; then
+    # Setup for the first conda installation
+    __conda_setup="$('/home-mscluster/sjumoorty2/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
     else
-        export PATH="/home-mscluster/sjumoorty2/anaconda3/bin:$PATH"
+        if [ -f "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/home-mscluster/sjumoorty2/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home-mscluster/sjumoorty2/anaconda3/bin:$PATH"
+        fi
     fi
-fi
-unset __conda_setup
-
-# If the above fails, try the alternative setup
-if ! command -v conda &> /dev/null; then
+    unset __conda_setup
+# If the first path doesn't exist, check the second conda path
+elif [ -d "/home-mscluster/mdawood/miniconda3" ]; then
+    # Setup for the second conda installation
     __conda_setup="$('/home-mscluster/mdawood/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__conda_setup"
@@ -26,6 +28,9 @@ if ! command -v conda &> /dev/null; then
         fi
     fi
     unset __conda_setup
+else
+    echo "Error: Neither conda installation found. Please check your conda paths."
+    exit 1
 fi
 
 cd ~/cleanrl/combined_exp
